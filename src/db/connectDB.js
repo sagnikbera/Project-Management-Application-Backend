3a7c83825c1fcc6b4edac3b5e1cb084b6bcf1750
sagnikbera <sagnikberaofficial@gmail.com>
2025-09-08 16:49:52 +0530

    MongoDB connection

--- src/db/connectDB.js
+++ src/db/connectDB.js
@@ -1,14 +1,13 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("MongoDB connected!");
-        
-    } catch (error) {
-        console.error("MongoDB connection ERROR" , error);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected!");
+  } catch (error) {
+    console.error("MongoDB connection ERROR", error);
+    process.exit(1);
+  }
 };
 
 export default connectDB;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,21 @@ dotenv.config({
   path: "./.env",
 });
 import app from "./app.js";
+import connectDB from "./db/connectDB.js";
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`App Listening on port http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`App Listening on port http://localhost:${port}`);
+// });
+
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`App Listening on port http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error!", err);
+    process.exit(1);
+  });
